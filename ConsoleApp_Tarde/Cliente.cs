599a5b3f3d55ab0111f353b0a7efdf4fe7337d5c
A0KAM1 <leticiarodrigues1603@hotmail.com>
2023-03-08 15:31:21 -0300

    Criação da classe Cliente com todos os atributos e métodos

---
+++ ConsoleApp_Tarde/Cliente.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Tarde
+{
+    public class Cliente
+    {
+        private int Id;
+        private string Nome;
+        private string Telefone;
+
+        public Cliente(int id, string nome, string telefone)
+        {
+            Id = id;
+            Name = nome;
+            Telefone = telefone;
+        }
+        //----------------------
+        public int getId()
+        {
+            return Id;
+        }
+        public string getNome()
+        {
+            return Nome;
+        }
+        public string getTelefone()
+        {
+            return Telefone;
+        }
+        //---------------------
+        public void setId(int id)
+        {
+            Id = id;
+        }
+        public void setNome(string nome)
+        {
+            Nome = nome;
+        }
+        public void setTelefone(string telefone)
+        {
+            Telefone = telefone;
+        }
+    }
+}
