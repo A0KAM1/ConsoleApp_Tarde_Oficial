8228205195c3cf5789bcdc222596b85a26458c2e
A0KAM1 <leticiarodrigues1603@hotmail.com>
2023-03-08 15:22:50 -0300

    Criação do método de set da classe Produto e mudança no Program

--- ConsoleApp_Tarde/Produto.cs
+++ ConsoleApp_Tarde/Produto.cs
@@ -8,16 +8,18 @@ namespace ConsoleApp_Tarde
 {
     public class Produto
     {
-        private int Id;
+        private int Id; //atributos
         private string Descricao;
         private decimal Valor;
 
-        public Produto(int id, string descricao, decimal valor)
+        public Produto(int id, string descricao, decimal valor) //método construtor, define que Atributo <- recebe parâmetro setado
         {
             Id = id;
             Descricao = descricao;
             Valor = valor;
         }
+
+        //Mostra valores
 
         public int getId()
         {
@@ -32,6 +34,26 @@ namespace ConsoleApp_Tarde
         public decimal getValor()
         {
             return Valor;
+        }
+
+        //Recebe valores
+
+        public void setId(int id)
+        {
+            Id = id;
+        }
+
+        public void setDescricao(string descricao)
+        {
+            Descricao = descricao;
+        }
+
+        public void setValor(decimal valor) //nao tem retorno, por isso é void
+        {
+            if (valor > 0)
+            {
+                Valor = valor;
+            }
         }
     }
 }
